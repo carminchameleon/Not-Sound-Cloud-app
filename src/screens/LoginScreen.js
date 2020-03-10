1475305d45b79b3b8f6e169ca0b8f52b00746c84
carminchameleon <hwanggee1211@gmail.com>
2020-03-10 21:55:01 +0900

    초기 세팅 중

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+export class LoginScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>HiHi</Text>
+      </View>
+    );
+  }
+}
+
+export default LoginScreen;
